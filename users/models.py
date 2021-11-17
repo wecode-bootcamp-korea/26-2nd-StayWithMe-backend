abf10ca0d45eb5fa9887ea0787044f02697f5526
Kyuuu827 <lck0827@gmail.com>
2021-11-17 17:41:10 +0900

    MODIFY: users models and bookings models

--- users/models.py
+++ users/models.py
@@ -5,9 +5,7 @@ from core.models import TimeStamp
 class User(TimeStamp):
     email        = models.CharField(max_length=100, unique=True, null=True)
     kakao_id     = models.CharField(max_length=40)
-    name         = models.CharField(max_length=40)
     nick_name    = models.CharField(max_length=40)
-    phone_number = models.CharField(max_length=15)
 
     class Meta:
         db_table = 'users'
